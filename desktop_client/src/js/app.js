71f4f2ded8b1d4e524e8a56a4c5d8afa10c0e5f6
bryk <piotrek.bryk@gmail.com>
2013-04-23 21:59:25 +0200

    Desktop app now displays Google Maps
    Modified makefile build script a lot
    Added Bootstrap lib
    Added Google Maps lib
    Introduced two *.html files. One for testing one for release.

--- desktop_client/src/js/app.js
+++ desktop_client/src/js/app.js
@@ -1,14 +1,25 @@
-goog.provide('io.app');
+'use strict';
+goog.provide('io');
+goog.provide('io.start');
 
 goog.require('goog.dom');
+goog.require('goog.events');
+goog.require('goog.net.jsloader');
 
-io.app = function() {
-  var newDiv = goog.dom.createDom('h1', {'style': 'background-color:#EEE'},
-      'Hello world!');
+io.start = function() {
+  var newDiv = goog.dom.createDom('h1', {'id': 'map_canvas'});
   goog.dom.appendChild(document.body, newDiv);
-  window.alert("hhhh");
+
+  var initialize = function() {
+    var mapOptions = {
+      center: new google.maps.LatLng(-34.397, 150.644),
+      zoom: 8,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    var map = new google.maps.Map(newDiv, mapOptions);
+  };
+  initialize();
 };
 
-// Ensures the symbol will be visible after compiler renaming.
-goog.exportSymbol('io.app', io.app);
+goog.exportSymbol('io.start', io.start);
 
