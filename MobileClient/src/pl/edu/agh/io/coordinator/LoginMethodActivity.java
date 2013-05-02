85d84492aace9cfa813b1872ca8581f1c5e88559
perexis <rafal@rafal>
2013-05-03 00:48:42 +0100

    Initial mobile client commit

---
+++ MobileClient/src/pl/edu/agh/io/coordinator/LoginMethodActivity.java
@@ -0,0 +1,22 @@
+package pl.edu.agh.io.coordinator;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class LoginMethodActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_login_method);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.login_method, menu);
+		return true;
+	}
+
+}
