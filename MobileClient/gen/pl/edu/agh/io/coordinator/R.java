8b65ae9f2e997e45b5cf767e364bb42b01465901
perexis <rafal@rafal>
2013-05-03 01:49:54 +0100

    Added login/password screen

--- MobileClient/gen/pl/edu/agh/io/coordinator/R.java
+++ MobileClient/gen/pl/edu/agh/io/coordinator/R.java
@@ -24,22 +24,42 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080001;
-        public static final int textView1=0x7f080000;
+        public static final int action_settings=0x7f08000a;
+        public static final int button1=0x7f080005;
+        public static final int buttonLogin=0x7f080009;
+        public static final int inputLogin=0x7f080007;
+        public static final int inputPassword=0x7f080008;
+        public static final int labelLogin=0x7f080006;
+        public static final int labelPassword=0x7f080000;
+        public static final int radioFingerprint=0x7f080004;
+        public static final int radioGroupLoginMethod=0x7f080001;
+        public static final int radioLoginPassword=0x7f080002;
+        public static final int radioNFC=0x7f080003;
     }
     public static final class layout {
         public static final int activity_login_method=0x7f030000;
-        public static final int activity_not_implemented_yet=0x7f030001;
+        public static final int activity_login_password=0x7f030001;
+        public static final int activity_not_implemented_yet=0x7f030002;
     }
     public static final class menu {
         public static final int login_method=0x7f070000;
-        public static final int not_implemented_yet=0x7f070001;
+        public static final int login_password=0x7f070001;
+        public static final int not_implemented_yet=0x7f070002;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
+        public static final int button_accept=0x7f050006;
+        public static final int button_login=0x7f05000d;
         public static final int hello_world=0x7f050002;
+        public static final int label_login=0x7f05000b;
+        public static final int label_login_method=0x7f050005;
+        public static final int label_password=0x7f05000c;
         public static final int label_we_are_sorry=0x7f050004;
+        public static final int radio_fingerprint=0x7f050009;
+        public static final int radio_login_password=0x7f050007;
+        public static final int radio_nfc=0x7f050008;
+        public static final int title_activity_login_password=0x7f05000a;
         public static final int title_activity_not_implemented_yet=0x7f050003;
     }
     public static final class style {
--- MobileClient/src/pl/edu/agh/io/coordinator/LoginMethodActivity.java
+++ MobileClient/src/pl/edu/agh/io/coordinator/LoginMethodActivity.java
@@ -2,7 +2,10 @@ package pl.edu.agh.io.coordinator;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.widget.RadioGroup;
 
 public class LoginMethodActivity extends Activity {
 
@@ -17,6 +20,25 @@ public class LoginMethodActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.login_method, menu);
 		return true;
+	}
+
+	public void clickLoginMethod(View view) {
+		Intent intent = null;
+		RadioGroup radioGroup = (RadioGroup) findViewById(R.id.radioGroupLoginMethod);
+		int option = radioGroup.getCheckedRadioButtonId();
+		switch (option) {
+		case R.id.radioLoginPassword:
+			intent = new Intent(LoginMethodActivity.this,
+					LoginPasswordActivity.class);
+			break;
+		case R.id.radioNFC:
+		case R.id.radioFingerprint:
+		default:
+			intent = new Intent(LoginMethodActivity.this,
+					NotImplementedYetActivity.class);
+
+		}
+		startActivity(intent);
 	}
 
 }
