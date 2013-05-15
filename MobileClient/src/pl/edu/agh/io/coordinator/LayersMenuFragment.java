813bc3da172413ed838b0b10e43337dbe2411846
Piotr Szmigielski <piti.pco@gmail.com>
2013-05-16 01:38:18 +0200

    implemented LayersMenuListAdapter for LayersMenuFragment

--- MobileClient/src/pl/edu/agh/io/coordinator/LayersMenuFragment.java
+++ MobileClient/src/pl/edu/agh/io/coordinator/LayersMenuFragment.java
@@ -1,22 +1,157 @@
 package pl.edu.agh.io.coordinator;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+import pl.edu.agh.io.coordinator.resources.Group;
+import pl.edu.agh.io.coordinator.resources.MapItem;
+import pl.edu.agh.io.coordinator.resources.User;
+import pl.edu.agh.io.coordinator.resources.UserItem;
+
 import android.app.Activity;
 import android.app.Fragment;
+import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.BaseExpandableListAdapter;
+import android.widget.TextView;
 
-/**
- * A simple {@link android.support.v4.app.Fragment} subclass. Activities that
- * contain this fragment must implement the
- * {@link LayersMenuFragment.OnFragmentInteractionListener} interface to handle
- * interaction events. Use the {@link LayersMenuFragment#newInstance} factory
- * method to create an instance of this fragment.
- * 
- */
 public class LayersMenuFragment extends Fragment {
+	
+	private class LayersMenuListAdapter extends BaseExpandableListAdapter {
+
+		private static final int ITEM_POSITION = 0;
+		private static final int USER_POSITION = 1;
+		private static final int GROUP_POSITION = 2;
+		
+		private Context context;
+		private List<String> menuGroups;
+		private List<UserItem> items;
+		private List<User> people;
+		private List<Group> groups;
+		
+		public LayersMenuListAdapter(Context context) {
+			this.context = context;
+			this.menuGroups = new ArrayList<String>();
+			this.menuGroups.add("Items");
+			this.menuGroups.add("People");
+			this.menuGroups.add("Groups");
+			this.items = new ArrayList<UserItem>();
+			this.people = new ArrayList<User>();
+			this.groups = new ArrayList<Group>();
+		}
+		
+		public void setItems(Set<UserItem> items) {
+			this.items = new ArrayList<UserItem>(items);
+		}
+		
+		public void setPeople(Set<User> people) {
+			this.people = new ArrayList<User>(people);
+		}
+		
+		public void setGroups(Set<Group> groups) {
+			this.groups = new ArrayList<Group>(groups);
+		}
+		
+		@Override
+		public boolean areAllItemsEnabled() {
+			return true;
+		}
+		
+		@Override
+		public Object getChild(int groupPosition, int childPosition) {
+			if (groupPosition == ITEM_POSITION) {
+				return items.get(childPosition);
+			} else if (groupPosition == USER_POSITION) {
+				return people.get(childPosition);
+			} else if (groupPosition == GROUP_POSITION) {
+				return groups.get(childPosition);
+			} else {
+				return null;
+			}
+		}
+		
+		@Override
+		public long getChildId(int groupPosition, int childPosition) {
+			return childPosition;
+		}
+		
+		@Override
+		public View getChildView(int groupPosition, int childPosition, boolean isLastChild, View convertView, ViewGroup parent) {
+			Object object = getChild(groupPosition, childPosition);
+			if (convertView == null) {
+				LayoutInflater layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+				convertView = layoutInflater.inflate(R.layout.child_layout, null);
+			}
+			TextView tv = (TextView) convertView.findViewById(R.id.tvChild);
+			String text = null;
+			if (groupPosition == ITEM_POSITION) {
+				text = ((UserItem) object).getId();
+			} else if (groupPosition == USER_POSITION) {
+				text = ((User) object).getId();
+			} else if (groupPosition == GROUP_POSITION) {
+				text = ((Group) object).getId();
+			}
+			tv.setText("   " + text);
+			return convertView;
+		}
+		
+		@Override
+	    public int getChildrenCount(int groupPosition) {
+			if (groupPosition == ITEM_POSITION) {
+				return items.size();
+			} else if (groupPosition == USER_POSITION) {
+				return people.size();
+			} else if (groupPosition == GROUP_POSITION) {
+				return groups.size();
+			} else {
+				return 0;
+			}
+	    }
+
+	    @Override
+	    public Object getGroup(int groupPosition) {
+	        return menuGroups.get(groupPosition);
+	    }
+
+	    @Override
+	    public int getGroupCount() {
+	        return 3;
+	    }
+
+	    @Override
+	    public long getGroupId(int groupPosition) {
+	        return groupPosition;
+	    }
+
+	    @Override
+		public View getGroupView(int groupPosition, boolean isExpanded, View convertView, ViewGroup parent) {
+			String group = (String) getGroup(groupPosition);
+			if (convertView == null) {
+				LayoutInflater layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+				convertView = layoutInflater.inflate(R.layout.group_layout, null);
+			}
+			TextView tv = (TextView) convertView.findViewById(R.id.tvGroup);
+			tv.setText(group);
+			return convertView;
+		}
+		
+	    @Override
+	    public boolean hasStableIds() {
+	        return true;
+	    }
+
+	    @Override
+	    public boolean isChildSelectable(int arg0, int arg1) {
+	        return true;
+	    }
+	    
+	}
+	
 	// TODO: Rename parameter arguments, choose names that match
 	// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 	private static final String ARG_PARAM1 = "param1";
