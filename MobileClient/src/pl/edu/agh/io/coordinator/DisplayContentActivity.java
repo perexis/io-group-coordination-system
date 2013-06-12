be6d5a1607ad1d950b6382f06857fa1764c0a0e7
perexis <perex@student.agh.edu.pl>
2013-06-12 23:32:04 +0100

    Fixed Content Activity

--- MobileClient/src/pl/edu/agh/io/coordinator/DisplayContentActivity.java
+++ MobileClient/src/pl/edu/agh/io/coordinator/DisplayContentActivity.java
@@ -19,93 +19,104 @@ import com.google.android.youtube.player.YouTubePlayerFragment;
 
 public class DisplayContentActivity extends Activity {
 
+	private ShowUserFragment userFragment;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_display_content);
+		if (savedInstanceState == null) {
+			Intent intent = getIntent();
 
-		Intent intent = getIntent();
+			ContentType contentType = ContentType.valueOf(intent
+					.getStringExtra(MainMapActivity.EXTRA_CONTENT_TYPE));
 
-		ContentType contentType = ContentType.valueOf(intent
-				.getStringExtra(MainMapActivity.EXTRA_CONTENT_TYPE));
+			FragmentManager fragmentManager = getFragmentManager();
+			FragmentTransaction fragmentTransaction = fragmentManager
+					.beginTransaction();
+			switch (contentType) {
+			case NOTE:
+				ShowNoteFragment noteFragment = ShowNoteFragment
+						.newInstance(intent
+								.getStringExtra(MainMapActivity.EXTRA_CONTENT));
+				fragmentTransaction.add(R.id.contentFrame, noteFragment);
+				break;
+			case IMAGE:
+				String imageUrl = intent
+						.getStringExtra(MainMapActivity.EXTRA_CONTENT);
+				if (!imageUrl.startsWith("http://"))
+					imageUrl = JSonProxy.SERVER_NAME + "/images/" + imageUrl;
+				ShowImageFragment imageFragment = ShowImageFragment
+						.newInstance(imageUrl);
+				fragmentTransaction.add(R.id.contentFrame, imageFragment);
+				break;
+			case VIDEO:
+				String videoUrl = intent
+						.getStringExtra(MainMapActivity.EXTRA_CONTENT);
+				if (!videoUrl.startsWith("http://")) {
+					videoUrl = JSonProxy.SERVER_NAME + "/videos/" + videoUrl;
+					ShowVideoFragment videoFragment = ShowVideoFragment
+							.newInstance(videoUrl);
+					fragmentTransaction.add(R.id.contentFrame, videoFragment);
+				} else {
+					final String videoID = new String(videoUrl);
+					YouTubePlayerFragment player = YouTubePlayerFragment
+							.newInstance();
+					player.initialize(DeveloperKey.YOUTUBE_DEVELOPER_KEY,
+							new OnInitializedListener() {
 
-		FragmentManager fragmentManager = getFragmentManager();
-		FragmentTransaction fragmentTransaction = fragmentManager
-				.beginTransaction();
-		switch (contentType) {
-		case NOTE:
-			ShowNoteFragment noteFragment = ShowNoteFragment.newInstance(intent
-					.getStringExtra(MainMapActivity.EXTRA_CONTENT));
-			fragmentTransaction.add(R.id.contentFrame, noteFragment);
-			break;
-		case IMAGE:
-			String imageUrl = intent
-					.getStringExtra(MainMapActivity.EXTRA_CONTENT);
-			if (!imageUrl.startsWith("http://"))
-				imageUrl = JSonProxy.SERVER_NAME + "/images/" + imageUrl;
-			ShowImageFragment imageFragment = ShowImageFragment
-					.newInstance(imageUrl);
-			fragmentTransaction.add(R.id.contentFrame, imageFragment);
-			break;
-		case VIDEO:
-			String videoUrl = intent
-					.getStringExtra(MainMapActivity.EXTRA_CONTENT);
-			if (!videoUrl.startsWith("http://")) {
-				videoUrl = JSonProxy.SERVER_NAME + "/videos/" + videoUrl;
-				ShowVideoFragment videoFragment = ShowVideoFragment
-						.newInstance(videoUrl);
-				fragmentTransaction.add(R.id.contentFrame, videoFragment);
-			} else {
-				final String videoID = new String(videoUrl);
-				YouTubePlayerFragment player = YouTubePlayerFragment
-						.newInstance();
-				player.initialize(DeveloperKey.YOUTUBE_DEVELOPER_KEY,
-						new OnInitializedListener() {
+								@Override
+								public void onInitializationSuccess(
+										Provider arg0, YouTubePlayer player,
+										boolean wasRestored) {
+									if (!wasRestored) {
+										Uri uri = Uri.parse(videoID);
+										String vlink = uri
+												.getQueryParameter("v");
+										player.setFullscreen(true);
+										player.cueVideo(vlink);
+									}
 
-							@Override
-							public void onInitializationSuccess(Provider arg0,
-									YouTubePlayer player, boolean wasRestored) {
-								if (!wasRestored) {
-									Uri uri = Uri.parse(videoID);
-									String vlink = uri.getQueryParameter("v");
-									player.setFullscreen(true);
-									player.cueVideo(vlink);
 								}
 
-							}
+								@Override
+								public void onInitializationFailure(
+										Provider arg0,
+										YouTubeInitializationResult arg1) {
+									Log.e("DisplayContentActivity",
+											"YouTube initialization failed");
 
-							@Override
-							public void onInitializationFailure(Provider arg0,
-									YouTubeInitializationResult arg1) {
-								Log.e("DisplayContentActivity",
-										"YouTube initialization failed");
+								}
+							});
+					fragmentTransaction.add(R.id.contentFrame, player);
+				}
 
-							}
-						});
-				fragmentTransaction.add(R.id.contentFrame, player);
+				break;
+			case USER:
+				String userName = intent
+						.getStringExtra(MainMapActivity.USER_NAME);
+				String userSurname = intent
+						.getStringExtra(MainMapActivity.USER_SURNAME);
+				String userNick = intent
+						.getStringExtra(MainMapActivity.USER_NICK);
+				String userPhone = intent
+						.getStringExtra(MainMapActivity.USER_PHONE);
+				String userMail = intent
+						.getStringExtra(MainMapActivity.USER_MAIL);
+				String userAvatar = intent
+						.getStringExtra(MainMapActivity.USER_AVATAR);
+				if (userFragment == null)
+					userFragment = ShowUserFragment.newInstance(userName,
+							userSurname, userNick, userPhone, userMail,
+							userAvatar);
+				fragmentTransaction.add(R.id.contentFrame, userFragment);
+
+				break;
+			default:
+				break;
 			}
-
-			break;
-		case USER:
-			String userName = intent.getStringExtra(MainMapActivity.USER_NAME);
-			String userSurname = intent
-					.getStringExtra(MainMapActivity.USER_SURNAME);
-			String userNick = intent.getStringExtra(MainMapActivity.USER_NICK);
-			String userPhone = intent
-					.getStringExtra(MainMapActivity.USER_PHONE);
-			String userMail = intent.getStringExtra(MainMapActivity.USER_MAIL);
-			String userAvatar = intent
-					.getStringExtra(MainMapActivity.USER_AVATAR);
-			ShowUserFragment userFragment = ShowUserFragment.newInstance(
-					userName, userSurname, userNick, userPhone, userMail,
-					userAvatar);
-			fragmentTransaction.add(R.id.contentFrame, userFragment);
-
-			break;
-		default:
-			break;
+			fragmentTransaction.commit();
 		}
-		fragmentTransaction.commit();
 
 	}
 
